2ecc38e4e363beac29fda677143d94c076a93283
Oblv49 <noprivacyworld@gmail.com>
2023-09-29 13:28:23 +0200

    add page detail

--- src/main/java/org/java/lessons/spring/blog/ricette/controller/IndexController.java
+++ src/main/java/org/java/lessons/spring/blog/ricette/controller/IndexController.java
@@ -3,12 +3,16 @@ package org.java.lessons.spring.blog.ricette.controller;
 import org.java.lessons.spring.blog.ricette.model.Recipe;
 import org.java.lessons.spring.blog.ricette.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -24,5 +28,17 @@ public class IndexController {
             model.addAttribute("messaggio", "Nessuna Ricetta disponibile.");
         }
         return "/recipe/list";
+    }
+
+    @GetMapping("/show/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if (recipeOptional.isPresent()) {
+            Recipe recipeFromDB = recipeOptional.get();
+            model.addAttribute("recipe", recipeFromDB);
+            return "/recipe/detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
